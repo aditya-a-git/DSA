27f105324f8482299fcdd3ebf236466a76f07cd7
Aditya A <adityaanil08@gmail.com>
2026-07-26 12:21:32 +0530

    feat: update LeetCode 55. Jump Game solution (cpp)

--- problems/jump-game/solution.cpp
+++ problems/jump-game/solution.cpp
@@ -1,29 +1,21 @@
 class Solution {
-    bool jump(vector<int>& nums, int i, vector<int>& dp) {
-        if (i >= nums.size()) {
-            return false;
-        }
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size() - 1;
+        int maxReach = 0;
 
-        if (i == nums.size() - 1) {
-            return true;
-        }
+        for (int i = 0; i <= n; i++) {
+            if (i > maxReach) {
+                return false;
+            }
 
-        if (dp[i] != -1) {
-            return dp[i];
-        }
+            maxReach = max(maxReach, i + nums[i]);
 
-        for (int j = 1; j <= nums[i]; j++) {
-            if (jump(nums, i + j, dp)) {
-                return dp[i] = true;
+            if (maxReach >= n) {
+                return true;
             }
         }
 
-        return dp[i] = false;
-    }
-
-public:
-    bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return jump(nums, 0, dp);
+        return false;
     }
 };
