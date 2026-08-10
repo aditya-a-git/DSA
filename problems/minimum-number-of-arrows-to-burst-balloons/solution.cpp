7d5dd874e0af9641fd0be228e3af68402a3b8c2e
Aditya A <adityaanil08@gmail.com>
2026-08-10 10:38:36 +0530

    feat: update LeetCode 452. Minimum Number of Arrows to Burst Balloons solution (cpp)

--- problems/minimum-number-of-arrows-to-burst-balloons/solution.cpp
+++ problems/minimum-number-of-arrows-to-burst-balloons/solution.cpp
@@ -6,10 +6,7 @@ public:
         int arrow = 0, ans = 0;
 
         for (const auto& vec : points) {
-            if (ans == 0) {
-                arrow = vec[1];
-                ans++;
-            } else if (arrow < vec[0] || arrow > vec[1]) {
+            if (ans == 0 || arrow < vec[0] || arrow > vec[1]) {
                 arrow = vec[1];
                 ans++;
             }
