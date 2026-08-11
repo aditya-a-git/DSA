f2db313bcebec4bff5bf8fbb6ad118f4115fb0d6
Aditya A <adityaanil08@gmail.com>
2026-08-11 13:02:31 +0530

    feat: add LeetCode 135. Candy solution (cpp)

---
+++ problems/candy/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        for (int i = 0; i < n; i++) {
+            if (i - 1 >= 0 && ratings[i] > ratings[i - 1] &&
+                candies[i] <= candies[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + 1 < n && ratings[i] > ratings[i + 1] &&
+                candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+
+        return accumulate(candies.begin(), candies.end(), 0);
+    }
+};
