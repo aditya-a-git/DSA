b16632326956c7901b2d737024a6759efb63f544
Aditya A <adityaanil08@gmail.com>
2026-08-21 20:20:36 +0530

    feat: update LeetCode 31. Next Permutation solution (cpp)

--- problems/next-permutation/solution.cpp
+++ problems/next-permutation/solution.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        // int n = nums.size();
-        // int idx;
+    void nextPermutation(vector<int>& nums) { // next_permutation(nums.begin(),
+                                              // nums.end()); obv works!
+        int n = nums.size();
+        int idx = -1;
 
-        // for(int i = n - 2; i >= 0; i--){
-        //     if(nums[i] < nums[i + 1]){
-        //         idx = i;
-        //         break;
-        //     }
-        // }
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break;
+            }
+        }
 
-        next_permutation(nums.begin(), nums.end());
+        if (idx == -1) {
+            sort(nums.begin(), nums.end());
+            return;
+        }
+
+        int swapidx = -1;
+
+        for (int i = idx + 1; i < n; i++) {
+            if (nums[i] > nums[idx]) {
+                if (swapidx == -1) {
+                    swapidx = i;
+                    continue;
+                }
+
+                if (nums[i] < nums[swapidx]) {
+                    swapidx = i;
+                }
+            }
+        }
+
+        swap(nums[idx], nums[swapidx]);
+        sort(nums.begin() + idx + 1, nums.end());
     }
 };
