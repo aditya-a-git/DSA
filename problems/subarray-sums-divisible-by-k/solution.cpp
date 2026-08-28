2f10917197e89f8e1808c80108975b137c11fca4
Aditya A <adityaanil08@gmail.com>
2026-08-28 19:07:48 +0530

    feat: add LeetCode 974. Subarray Sums Divisible by K solution (cpp)

---
+++ problems/subarray-sums-divisible-by-k/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int count = 0;
+        int prefix = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefix += nums[i];
+            int rem = prefix % k;
+
+            if(rem < 0){
+                rem += k;
+            }
+
+            count += mp[rem];
+            mp[rem]++;
+        }
+
+        return count;
+    }
+};
