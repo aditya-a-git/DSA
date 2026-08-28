ba28a8055a69d6fc35533861c0f08963a1a025b8
Aditya A <adityaanil08@gmail.com>
2026-08-28 13:17:54 +0530

    feat: update LeetCode 930. Binary Subarrays With Sum solution (cpp)

--- problems/binary-subarrays-with-sum/solution.cpp
+++ problems/binary-subarrays-with-sum/solution.cpp
@@ -2,33 +2,19 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int n = nums.size();
-        vector<int> prefix(n);
-        prefix[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
-        }
-
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int prefix = 0;
         int count = 0;
 
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (i - 1 < 0) {
-                    if (prefix[j] == goal) {
-                        count++;
-                    }
-                } else {
-                    int sum = prefix[j] - prefix[i - 1];
+            prefix += nums[i];
 
-                    if (sum == goal) {
-                        count++;
-                    }
-
-                    if (sum > goal) {
-                        break;
-                    }
-                }
+            if(mp.find(prefix - goal) != mp.end()){
+                count += mp[prefix - goal];
             }
+
+            mp[prefix]++;
         }
 
         return count;
