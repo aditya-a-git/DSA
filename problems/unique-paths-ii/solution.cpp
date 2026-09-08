6c2baee5279a7522fca6a53524c30444fafa116f
Aditya A <adityaanil08@gmail.com>
2026-09-08 17:11:22 +0530

    feat: add LeetCode 63. Unique Paths II solution (cpp)

---
+++ problems/unique-paths-ii/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int solve(vector<vector<int>>& grid, int i, int j, int m, int n,
+              vector<vector<int>>& dp) {
+        if (i >= m || j >= n || grid[i][j] == 1) {
+            return 0;
+        }
+
+        if (i == m - 1 && j == n - 1) {
+            return grid[i][j] == 0 ? 1 : 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        return dp[i][j] = solve(grid, i + 1, j, m, n, dp) + solve(grid, i, j + 1, m, n, dp);
+    }
+
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(obstacleGrid, 0, 0, m, n, dp);
+    }
+};
