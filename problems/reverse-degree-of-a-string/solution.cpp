e6523d962756120c9c219576c86c61d382bf2b20
Aditya A <adityaanil08@gmail.com>
2026-09-20 12:02:00 +0530

    feat: add LeetCode 3498. Reverse Degree of a String solution (cpp)

---
+++ problems/reverse-degree-of-a-string/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+        int n = s.size();
+
+        for(int i = 1; i <= n; i++){
+            sum += ('z' - s[i - 1] + 1) * i;
+        }
+
+        return sum;
+    }
+};
